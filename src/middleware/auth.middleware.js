b32d56b4a75d13f82e2645abe27ba2019ad29297
Khushpreet Singh <khushramgaria@gmail.com>
2024-07-23 17:17:54 +0530

    employee login and register

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,65 @@
+import jwt from "jsonwebtoken"
+import { Employee } from "../model/employee.model.js";
+import { Admin } from "../model/admin.model.js";
+
+export const adminVerifyJWT = async(req, res, next) => {
+    console.log(req.header('Authorization'))
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization").replace("Bearer ", "")
+
+        if (!token) {
+            return res.status(400).json({
+                message: "Unauthorized request !!",
+                success: false,
+            })
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const admin = await Admin.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if (!admin) {
+            return res.status(400).json({
+                message: "Access Token not found",
+                success: false,
+            })
+        }
+
+        req.admin = admin
+        next()
+
+    } catch (error) {
+        return res.status(400).json({
+            message: "Invalid access token",
+            success: false,
+        })
+    }
+}
+
+export const employeeVerifyJWT = async(req, res, next) => {
+    console.log(req.header('Authorization'))
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization").replace("Bearer ", "")
+
+        if (!token) {
+            return res.status(400).json({
+                message: "Unauthorized request !!",
+                success: false,
+            })
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await Employee.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if (!user) {
+            throw new ApiError(400, "Access token not found !! ")
+        }
+
+        req.user = user
+        next()
+
+    } catch (error) {
+        throw new ApiError(401, "Invalid Access Token")
+    }
+}
---
+++ src/model/employee.model.js
@@ -0,0 +1,75 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const employeeSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        employeeId: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        refreshToken: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+employeeSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+employeeSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+employeeSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            name: this.name,
+            employeeId: this.employeeId,
+            position: this.position
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+employeeSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const Employee = mongoose.model("Employee", employeeSchema)
