534d44506c95e290d687da198c5e988bea9e917d
Khushpreet Singh <khushramgaria@gmail.com>
2024-08-06 15:38:05 +0530

    config model

---
+++ src/controllers/config.controller.js
@@ -0,0 +1,5 @@
+import Config from "./../model/config.model.js"
+
+const employeeRole = async( req, res) => {
+    const {  } = req.body
+}
---
+++ src/model/config.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const holidaySchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+const employeeRoleSchema = new mongoose.Schema({
+  value: {
+    type: String,
+    required: true
+  }
+})
+
+const configSchema = new mongoose.Schema({
+  holidays: [holidaySchema],
+  options: [employeeRoleSchema]
+})
+
+const Config = mongoose.model('Config', configSchema)
+
+module.exports = Config;
