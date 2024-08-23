4494a89aabb38374d78735030676905b0ee15209
jassijasit <jasveersingh182002@gmail.com>
2024-08-23 13:05:30 +0530

    change a url fro sockect .io

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://bringletech-crm-frontend.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
